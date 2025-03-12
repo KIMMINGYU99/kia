a0fd169aaeaebb8d9ce38134104f892fdd2207d9
KIMMINGYU <kmk4604@gmail.com>
2025-03-12 17:47:47 +0900

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { Route, Routes } from "react-router";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Inspection } from "./pages/Inspection";
 
 import { CarDetail } from "./pages/CarDetail";
 
 function App() {
   return (
-    <Routes>
-      {/* 정적 라우팅 */}
-      <Route path={"/"} element={<Inspection />} />
+    <Router>
+      <Routes>
+        {/* 정적 라우팅 */}
+        <Route path={"/"} element={<Inspection />} />
 
-      {/* 동적 라우팅 */}
-      <Route path={"/detail/:id"} element={<CarDetail />} />
-    </Routes>
+        {/* 동적 라우팅 */}
+        <Route path={"/detail/:id"} element={<CarDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
